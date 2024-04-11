fa86f74ebbb9aff1fe66c60f236f5085c55c0e94
Benjamin Rühl <benjaminruehl@gmail.com>
2024-04-11 14:26:19 +0200

    Remove strange static field

--- lib/src/model/territory.dart
+++ lib/src/model/territory.dart
@@ -2,7 +2,6 @@ import 'package:appstore_connect/src/model/model.dart';
 
 class Territory extends Model {
   static const String type = 'territories';
-  static final Territory USA = Territory('USA', {'currency': 'USD'});
 
   final String id;
   final String currency;
